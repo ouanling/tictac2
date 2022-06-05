4e8e5228fca48108ce710d4472d758a4eb185514
ouanling <rkt1337@gmail.com>
2022-06-05 11:03:26 -0400

    added 5 round and scoring

--- javascript.js
+++ javascript.js
@@ -1,20 +1,23 @@
-function PlayRound(PlayerSelection, CPUSelection) {
-    
-    if (PlayerSelection == CPUSelection) { return "tie" }
-    else if (WinCombo[PlayerSelection] == CPUSelection) { return `You win ${PlayerSelection} beats ${CPUSelection}`
-        
-    }
-    else {
-        return `You lose ${PlayerSelection} loses to ${CPUSelection}`
-    }
+let RoundResult;
 
+function PlayRound(PlayerSelection, CPUSelection) {
+  if (PlayerSelection == CPUSelection) {
+    console.log( "tie");
+    RoundResult = 0;
+  } else if (WinCombo[PlayerSelection] == CPUSelection) {
+    console.log( `You win ${PlayerSelection} beats ${CPUSelection}`);
+    RoundResult = "Pscore" ;
+  } else {
+    console.log( `You lose ${PlayerSelection} loses to ${CPUSelection}`);
+    RoundResult = "CPUscore";
+  }
 }
 function ComputerPlay() {
-let choice = ['rock', 'paper', 'scissor'];
-return choice[Math.floor(choice.length * Math.random())];
+  let choice = ["rock", "paper", "scissor"];
+  return choice[Math.floor(choice.length * Math.random())];
 }
 function GetChoice() {
-   return prompt("rock paper or scissor").toLowerCase();
+  return prompt("rock paper or scissor").toLowerCase();
 }
 const WinCombo = {
   rock: "scissor",
@@ -23,3 +26,17 @@ const WinCombo = {
 
   scissor: "paper",
 };
+
+function game() {
+    let Pscore = 0, CPUscore = 0;
+    for (let index = 0; index < 5; index++) {
+    PlayRound(GetChoice(),ComputerPlay());
+    if (RoundResult) {
+        if (RoundResult == "Pscore") { Pscore++}
+        else {CPUscore++}
+    }
+        
+    }
+    console.log(Pscore,CPUscore);
+    Pscore==CPUscore ? console.log("you tied") : Pscore>CPUscore ? console.log("Player won" ) : console.log("CPU won");
+}
