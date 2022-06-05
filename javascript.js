fc51810ae5f1e17d3f6e74afc70de785b177d667
ouanling <rkt1337@gmail.com>
2022-06-05 11:12:31 -0400

    prettyd the code

--- javascript.js
+++ javascript.js
@@ -2,13 +2,13 @@ let RoundResult;
 
 function PlayRound(PlayerSelection, CPUSelection) {
   if (PlayerSelection == CPUSelection) {
-    console.log( "tie");
+    console.log("tie");
     RoundResult = 0;
   } else if (WinCombo[PlayerSelection] == CPUSelection) {
-    console.log( `You win ${PlayerSelection} beats ${CPUSelection}`);
-    RoundResult = "Pscore" ;
+    console.log(`You win ${PlayerSelection} beats ${CPUSelection}`);
+    RoundResult = "Pscore";
   } else {
-    console.log( `You lose ${PlayerSelection} loses to ${CPUSelection}`);
+    console.log(`You lose ${PlayerSelection} loses to ${CPUSelection}`);
     RoundResult = "CPUscore";
   }
 }
@@ -28,15 +28,22 @@ const WinCombo = {
 };
 
 function game() {
-    let Pscore = 0, CPUscore = 0;
-    for (let index = 0; index < 5; index++) {
-    PlayRound(GetChoice(),ComputerPlay());
+  let Pscore = 0,
+    CPUscore = 0;
+  for (let index = 0; index < 5; index++) {
+    PlayRound(GetChoice(), ComputerPlay());
     if (RoundResult) {
-        if (RoundResult == "Pscore") { Pscore++}
-        else {CPUscore++}
+      if (RoundResult == "Pscore") {
+        Pscore++;
+      } else {
+        CPUscore++;
+      }
     }
-        
-    }
-    console.log(Pscore,CPUscore);
-    Pscore==CPUscore ? console.log("you tied") : Pscore>CPUscore ? console.log("Player won" ) : console.log("CPU won");
+  }
+  console.log(Pscore, CPUscore);
+  Pscore == CPUscore
+    ? console.log("you tied")
+    : Pscore > CPUscore
+    ? console.log("Player won")
+    : console.log("CPU won");
 }
